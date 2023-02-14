e224f854bf31043c4ed44225144be2410b3ade8e
4geru <westhouse51@gmail.com>
2023-02-14 20:21:35 +0900

    display microcms lists

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,34 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
-import Date from '../components/date'
+import { microcmsClient } from "../lib/microcmsClient";
 
-export default function Home({ allPostsData }) {
+export default function Home({ staffs }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
+      <div>
+        <ul>
+          {staffs.map((staff) => (
+            <li key={staff.id}>
+              <Link href={`/blog/${staff.id}`}>{staff.staffName}</Link>
             </li>
           ))}
         </ul>
-      </section>
+      </div>
     </Layout>
   )
 }
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async () => {
+  const data = await microcmsClient.get({ endpoint: "staffs" });
+
   return {
     props: {
-      allPostsData
-    }
-  }
-}
+      staffs: data.contents,
+    },
+  };
+};
