78104d03fc1b5c5924c0116a60c22f57ca85d091
4geru <westhouse51@gmail.com>
2023-02-19 00:25:10 +0900

    remove posts

---
+++ components/staffLayout.js
@@ -0,0 +1,74 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Script from 'next/script'
+
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+export const siteTitle = 'Next.js Sample Website'
+
+export default function StaffLayout({ children, staff }) {
+  console.log(staff)
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <header className={styles.header}>
+        <Link href="/">
+          {
+            staff.photo?.url ?
+            <Image
+              priority
+              src={staff.photo.url}
+              className={utilStyles.borderCircle}
+              height={108}
+              width={108}
+              alt={staff.staffName}
+            /> :
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={108}
+              width={108}
+              alt={staff.staffName}
+            />
+          }
+          
+        </Link>
+        <h2 className={utilStyles.headingLg}>
+          <Link href="/" className={utilStyles.colorInherit}>
+            {staff.staffName}
+          </Link>
+        </h2>
+      </header>
+      <main>{children}</main>
+      {(
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+    </div>
+  )
+}
