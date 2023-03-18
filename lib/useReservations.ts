3fc011b2639af593ae771997ac69542df9830554
4geru <westhouse51@gmail.com>
2023-03-18 19:49:17 +0900

    sort method order

--- lib/useReservations.ts
+++ lib/useReservations.ts
@@ -10,24 +10,24 @@ export interface Reservation {
   staffFreeForm: string;
 }
 
-export const getReservations = async (microcmsClient, filters: string, success: () => {}) => {
-  // 「予約一覧」のコードを追加する
-  return [];
-}
-
 export const createReservation = (microcmsClient, reservation: Reservation, staff: Staff, success = () => {}) => {
   if(!confirm("予約しますか？"))return;
   // 「予約作成」のコードを追加する
   return reservation;
 }
 
-export const deleteReservation = (microcmsClient, reservation: Reservation, success: () => {}) => {
-  if(!confirm("予約を解除しますか？"))return;
-  // 「予約削除」のコードを追加する
-  return reservation;
+export const getReservations = async (microcmsClient, filters: string, success: () => {}) => {
+  // 「予約一覧」のコードを追加する
+  return [];
 }
 
 export const updateReservation = (microcmsClient, reservation: Reservation, success = () => {}) => {
   // 「予約メモの更新」のコードを追加する
+  return reservation;
+}
+
+export const deleteReservation = (microcmsClient, reservation: Reservation, success: () => {}) => {
+  if(!confirm("予約を解除しますか？"))return;
+  // 「予約削除」のコードを追加する
   return reservation;
 }
