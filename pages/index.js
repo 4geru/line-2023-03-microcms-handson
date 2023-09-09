da5caf27e327a9c070dd110b9ae1eba26fa21d23
Takuro Anzuchi <dfre0821@gmail.com>
2023-09-09 16:58:03 +0900

    delete button

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,55 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Layout, { siteTitle } from '../components/layout'
-import Link from 'next/link'
+import Head from "next/head";
+import Image from "next/image";
+import Layout, { siteTitle } from "../components/layout";
+import Link from "next/link";
 import { LiffContext } from "./_app";
 import { createMicrocmsClient } from "../lib/microcmsClient";
-import { createRandomStaff, createStaff } from "../lib/useStaff"
-import { deleteReservation } from "../lib/useReservations"
-import { useState, useContext, useEffect } from 'react';
-import { List, ListItem, IconButton, Button, Container, Snackbar, Alert } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { createRandomStaff, createStaff } from "../lib/useStaff";
+import { deleteReservation } from "../lib/useReservations";
+import { useState, useContext, useEffect } from "react";
+import {
+  List,
+  ListItem,
+  IconButton,
+  Button,
+  Container,
+  Snackbar,
+  Alert,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { lineNotify } from "../lib/lineNotify";
-import styles from '../components/layout.module.css'
-import utilStyles from '../styles/utils.module.css'
+import styles from "../components/layout.module.css";
+import utilStyles from "../styles/utils.module.css";
 import { getPreviousReservations, dateToString } from "../lib/util";
 
 export default function Home({ _staffs, serviceDomain, apiKey }) {
   const { liffObject: liff, profile, setLiffState } = useContext(LiffContext);
 
-  const [staffs, setStaff] = useState(_staffs)
-  const [reservations, setReservation] = useState([])
-  const [snackMessage, setSnackMessage] = useState(undefined)
+  const [staffs, setStaff] = useState(_staffs);
+  const [reservations, setReservation] = useState([]);
+  const [snackMessage, setSnackMessage] = useState(undefined);
 
   const microcmsClient = createMicrocmsClient({
     serviceDomain: serviceDomain,
-    apiKey: apiKey
-  })
+    apiKey: apiKey,
+  });
   // reference: https://document.microcms.io/content-api/get-list-contents#hf768a2fd4d
   useEffect(() => {
-    microcmsClient.get({
-      endpoint: "reservations",
-      queries: { limit: 20, filters: `lineId[equals]${profile?.userId}`, orders: '-reservationAt' },
-    }).then((data) => {
-      setReservation(data.contents)
-    })
-  }, [profile])
+    microcmsClient
+      .get({
+        endpoint: "reservations",
+        queries: {
+          limit: 20,
+          filters: `lineId[equals]${profile?.userId}`,
+          orders: "-reservationAt",
+        },
+      })
+      .then((data) => {
+        setReservation(data.contents);
+      });
+  }, [profile]);
 
-  const previousReservations = getPreviousReservations(reservations)
+  const previousReservations = getPreviousReservations(reservations);
 
   return (
     <Layout home>
@@ -54,17 +68,18 @@ export default function Home({ _staffs, serviceDomain, apiKey }) {
           />
           <h1 className={utilStyles.headingMd}>{profile.displayName}</h1>
           <p className={utilStyles.lightText}>
-            こんにちは、{profile.displayName}さん、しげサロンへようこそ！<br/>
-
-            {
-              previousReservations.length != 0 ?
+            こんにちは、{profile.displayName}さん、しげサロンへようこそ！
+            <br />
+            {previousReservations.length != 0 ? (
               <>
                 来店ポイント {previousReservations.length} pt です。
-                前回の来店は {dateToString(previousReservations[0].reservationAt)} です。
-              </> :
+                前回の来店は{" "}
+                {dateToString(previousReservations[0].reservationAt)} です。
+              </>
+            ) : (
               `はじめまして、お客様に合った最高のヘアスタイルをご提供できるよう、スタッフ一同心よりお待ちしております。`
-            }
-            <br/>
+            )}
+            <br />
             <br />
           </p>
         </header>
@@ -79,16 +94,18 @@ export default function Home({ _staffs, serviceDomain, apiKey }) {
                 <IconButton
                   onClick={() => {
                     // workshop: deleteStaff を追加しましょう
-                    if(!confirm("スタッフを削除しますか？"))return;
+                    if (!confirm("スタッフを削除しますか？")) return;
                     microcmsClient
                       .delete({
-                        endpoint: 'staffs',
+                        endpoint: "staffs",
                         contentId: staff.id,
                       })
                       .then(() => {
-                        const newStaffs = staffs.filter((_staff) => _staff.id != staff.id)
-                        setStaff(newStaffs)
-                        setSnackMessage(`${staff.staffName}を削除しました`)
+                        const newStaffs = staffs.filter(
+                          (_staff) => _staff.id != staff.id
+                        );
+                        setStaff(newStaffs);
+                        setSnackMessage(`${staff.staffName}を削除しました`);
                       })
                       .catch((err) => console.error(err));
                   }}
@@ -102,90 +119,94 @@ export default function Home({ _staffs, serviceDomain, apiKey }) {
             </ListItem>
           ))}
         </List>
-        <Button
-          variant="contained"
-          onClick={() => {
-            const staff = createRandomStaff()
-            createStaff(microcmsClient, (res) => {
-              setStaff([{ id: res.id, ...staff }, ...staffs])
-            }, staff);
-            setSnackMessage(`${staff.staffName}を追加しました`)
-          }}
-        >
-          ハンズオン用にmicroCMS上に新規スタッフ作成
-        </Button>
       </Container>
 
       <Container>
-        { profile ?
+        {profile ? (
           <Button
             variant="contained"
-            color='error'
+            color="error"
             onClick={() => {
-              liff.logout()
-              setLiffState([liff, null])
+              liff.logout();
+              setLiffState([liff, null]);
             }}
-          >ログアウト</Button> :
+          >
+            ログアウト
+          </Button>
+        ) : (
           <Button
             variant="contained"
             onClick={() => {
-              liff.login({})
+              liff.login({});
             }}
-          >ログイン</Button>
-        }
+          >
+            ログイン
+          </Button>
+        )}
       </Container>
 
-      { /* ログイン済み */
-        profile && <Container>
-          <h2>予約一覧</h2>
-          <List>
-            {reservations.map((reservation) => (
-              <ListItem
-                key={reservation.id}
-                secondaryAction={
-                  <IconButton
-                    onClick={() => {
-                      deleteReservation(microcmsClient, reservation, () => {
-                        const date = dateToString(reservation.reservationAt)
-                        const staffName = reservation.staff.staffName;
-                        const message = `${staffName}さん：${reservation.userName}様の${date}からの予約削除がされました。`
-                        const userMessage = `${date}からの予約削除がされました。`
-                        lineNotify(message);
-                        setSnackMessage(userMessage);
-                        const _reservations = reservations.filter((e) => e !== reservation)
-                        setReservation(_reservations)
-                      });
-                    }}
-                    aria-label=""
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                }
-              >
-                <Link href={`/reservations?id=${reservation.id}`}>
-                  {reservation.staff?.staffName}: {dateToString(reservation.reservationAt)}
-                </Link>
-              </ListItem>
-            ))}
-          </List>
-        </Container>
+      {
+        /* ログイン済み */
+        profile && (
+          <Container>
+            <h2>予約一覧</h2>
+            <List>
+              {reservations.map((reservation) => (
+                <ListItem
+                  key={reservation.id}
+                  secondaryAction={
+                    <IconButton
+                      onClick={() => {
+                        deleteReservation(microcmsClient, reservation, () => {
+                          const date = dateToString(reservation.reservationAt);
+                          const staffName = reservation.staff.staffName;
+                          const message = `${staffName}さん：${reservation.userName}様の${date}からの予約削除がされました。`;
+                          const userMessage = `${date}からの予約削除がされました。`;
+                          lineNotify(message);
+                          setSnackMessage(userMessage);
+                          const _reservations = reservations.filter(
+                            (e) => e !== reservation
+                          );
+                          setReservation(_reservations);
+                        });
+                      }}
+                      aria-label=""
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  }
+                >
+                  <Link href={`/reservations?id=${reservation.id}`}>
+                    {reservation.staff?.staffName}:{" "}
+                    {dateToString(reservation.reservationAt)}
+                  </Link>
+                </ListItem>
+              ))}
+            </List>
+          </Container>
+        )
       }
 
       {
         <Snackbar
           open={!!snackMessage}
           anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'right',
+            vertical: "bottom",
+            horizontal: "right",
           }}
         >
-          <Alert onClose={()=>{setSnackMessage(undefined)}} severity="success">
+          <Alert
+            onClose={() => {
+              setSnackMessage(undefined);
+            }}
+            severity="success"
+          >
             {snackMessage}
           </Alert>
         </Snackbar>
       }
     </Layout>
-  )
+  );
 }
 
 // データをテンプレートに受け渡す部分の処理を記述します
@@ -201,7 +222,7 @@ export const getStaticProps = async () => {
       _staffs: staffsData.contents,
       serviceDomain: process.env.SERVICE_DOMAIN,
       apiKey: process.env.MICROCMS_API_KEY,
-      liffId: process.env.LIFF_ID
+      liffId: process.env.LIFF_ID,
     },
   };
 };
